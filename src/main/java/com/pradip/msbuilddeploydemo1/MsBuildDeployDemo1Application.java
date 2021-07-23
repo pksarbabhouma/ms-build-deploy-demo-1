b6dbe3e60fdd16e8249572270b1ccc7aad8f63ca
pradip ks <contact.pradipks@gmail.com>
2021-07-23 07:14:38 +0530

    Demo project created

---
+++ src/main/java/com/pradip/msbuilddeploydemo1/MsBuildDeployDemo1Application.java
@@ -0,0 +1,14 @@
+package com.pradip.msbuilddeploydemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBuildDeployDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBuildDeployDemo1Application.class, args);
+		System.out.println("Here is my first Spring Boot Microservice");
+	}
+
+}
---
+++ src/test/java/com/pradip/msbuilddeploydemo1/MsBuildDeployDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradip.msbuilddeploydemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBuildDeployDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
